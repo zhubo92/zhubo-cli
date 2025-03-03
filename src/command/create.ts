b09175a117af48e32dc02103ffd7e0b8ba455460
朱波 <zhubo0327@gmail.com>
2025-03-03 09:29:15 +0800

    feat: 检查版本更新

--- src/command/create.ts
+++ src/command/create.ts
@@ -2,6 +2,10 @@ import {input, select} from "@inquirer/prompts";
 import {clone} from "../utils/clone";
 import path from "node:path";
 import fs from "fs-extra";
+import {name, version} from "../../package.json";
+import axios, {AxiosResponse} from "axios";
+import {gt} from "lodash";
+import chalk from "chalk";
 
 export interface ITemplateInfo {
     name: string; // 模板名称
@@ -36,22 +40,54 @@ export async function isOverwrite(filename: string) {
     })
 }
 
+// 获取远端最新版本号
+async function getNpmLatestVersion(name: string) {
+    const npmUrl = `https://registry.npmjs.org/${name}`;
+    try {
+        const res = (await axios.get(npmUrl))as AxiosResponse<{"dist-tags": { latest: string }}>;
+        return res.data["dist-tags"].latest;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+async function checkVersion(name: string, version: string) {
+    const latestVersion = await getNpmLatestVersion(name);
+    if(!latestVersion) {
+        console.error(chalk.redBright("检测不到远端最新版本，请检查本地连接"));
+        return;
+    }
+    const isNeedUpdate = gt(latestVersion, version);
+    if(isNeedUpdate) {
+        console.warn(`检查到 zhubo-cli 最新版本为 ${chalk.blackBright(latestVersion)}，当前版本为 ${chalk.blackBright(version)}`);
+        console.warn(`可使用 ${chalk.yellow("npm install zhubo-cli@latest")}，或者使用 ${chalk.yellow("zhubo update")} 更新`);
+    }
+}
+
 export async function create(projectName?: string) {
-    if(!projectName) {
+    // 检查版本更新
+    await checkVersion(name, version);
+
+    if (!projectName) {
         projectName = await input({message: "请输入项目名称"});
     }
 
     // 如果文件夹存在，则提示是否覆盖
     const filePath = path.resolve(process.cwd(), projectName);
-    if(fs.existsSync((filePath))) {
+    if (fs.existsSync((filePath))) {
         const overwriteResult = await isOverwrite(projectName);
         // 不覆盖直接结束
-        if(!overwriteResult) return;
+        if (!overwriteResult) return;
         fs.removeSync(filePath);
     }
 
     // 初始化模板列表
-    const templateList = Array.from(templates).map(([name, info]: [string, ITemplateInfo]) => ({name, value: name, description: info.description}));
+    const templateList = Array.from(templates).map(([name, info]: [string, ITemplateInfo]) => ({
+        name,
+        value: name,
+        description: info.description
+    }));
 
     const selectedTemplateName = await select({
         message: "请选择模板",
@@ -59,7 +95,7 @@ export async function create(projectName?: string) {
     });
 
     const selectedTemplateInfo = templates.get(selectedTemplateName);
-    if(!selectedTemplateInfo) return;
+    if (!selectedTemplateInfo) return;
 
     // 下载
     await clone(selectedTemplateInfo!.downloadUrl, projectName, ["-b", selectedTemplateInfo!.branch]);
